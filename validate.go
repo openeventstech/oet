8b03adb3a4cd209e0ddd9698b918fc6e46cbdaf2
Fabio Alessandro Locati <me@fale.io>
2024-11-17 17:29:48 +0100

    Allow to get all errors

--- validate.go
+++ validate.go
@@ -5,6 +5,7 @@ import (
 	"log/slog"
 	"os"
 	"path/filepath"
+	"sync"
 
 	"github.com/spf13/cobra"
 	"gopkg.in/yaml.v3"
@@ -21,6 +22,13 @@ var validateCmd = &cobra.Command{
 	Run:   validate,
 }
 
+var chErrors chan vError
+
+type vError struct {
+	Path  string
+	Error error
+}
+
 func validate(cmd *cobra.Command, args []string) {
 	// Load schemas
 	err := loadSchemas()
@@ -29,12 +37,37 @@ func validate(cmd *cobra.Command, args []string) {
 		os.Exit(1)
 	}
 
+	// Setup the co-routine to handle validation errors from a channel
 	var errors int
+	chErrors = make(chan vError)
+	chErrorsDone := make(chan bool)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case <-chErrorsDone:
+				return
+			case err := <-chErrors:
+				errors++
+				slog.Info("file validity",
+					slog.String("path", err.Path),
+					slog.Any("error", err.Error),
+				)
+			}
+		}
+	}()
+
 	// Validate files
-	instanceFile := args[0]
-	if err = filepath.Walk(instanceFile, validateFile); err != nil {
-		errors++
+	if err = filepath.Walk(args[0], validateFile); err != nil {
+		slog.Error(err.Error())
+		return
 	}
+
+	// Post-file validation
+	chErrorsDone <- true
+	wg.Wait()
 	slog.Info("errors count", slog.Int("errors", errors))
 }
 
@@ -50,7 +83,7 @@ func validateFile(path string, info os.FileInfo, err error) error {
 		slog.Error(err.Error())
 		os.Exit(1)
 	}
-	item := make(map[string]interface{})
+	item := make(map[string]any)
 	if err = yaml.Unmarshal([]byte(fc), &item); err != nil {
 		slog.Error(err.Error())
 		os.Exit(1)
@@ -72,7 +105,10 @@ func validateFile(path string, info os.FileInfo, err error) error {
 	)
 
 	if err != nil {
-		slog.Info("invalid file", slog.Any("error", err))
+		chErrors <- vError{
+			Path:  path,
+			Error: err,
+		}
 	}
-	return err
+	return nil
 }
